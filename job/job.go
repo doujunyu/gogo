9781aaef04a1dd0bb86264ad684e048c8b0e64c0
doujunyu <2457913616@qq.com>
2022-07-22 10:11:04 +0800

    Update job.go

--- job/job.go
+++ job/job.go
@@ -10,6 +10,7 @@ type Job struct {
 	R         *http.Request
 	File      *Files                 `Testing:"文件"`
 	Input     map[string]string      `Testing:"接收的参数"`
+	InputJson string                 `Testing:"接收的json数据"`
 	GroupData map[string]interface{} `Testing:"跨方法的数据"`
 	IsFlow    bool                   `Testing:"判断是否继续向下执行:true=继续执行,false=停止"`
 }
